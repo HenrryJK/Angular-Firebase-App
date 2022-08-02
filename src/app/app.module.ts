b0471d80c4c05b440c99bdc66ffb2dca84a10607
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-08-02 17:22:59 -0500

    #1 Configuracion de Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// importamos las clases para trabajar con firestore
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+
+// importamos  la configuracion del firebase
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // declarando las importaciones
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBaOYvawcaWo_Rmof74Tr8nmn_A3YtI_Z0",
+    authDomain: "crud-angular-firebase-01.firebaseapp.com",
+    projectId: "crud-angular-firebase-01",
+    storageBucket: "crud-angular-firebase-01.appspot.com",
+    messagingSenderId: "111702727205",
+    appId: "1:111702727205:web:c88e73c8f265fd9ec6fdcc"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
