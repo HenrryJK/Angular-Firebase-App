045de755c9ab18d011c10bfa264b25b9548264ce
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-08-04 18:21:56 -0500

    #3 Creando rutas , componentes, funcionalidad de algunos botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+/// impotamos los componentes creamos
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ShowComponent } from './components/show/show.component';
+const routes: Routes = [
+  {path:'', component:ShowComponent},
+  {path:'create', component:CreateComponent},
+  {path:'edit/:id', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/show/show.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+// Importamos el modelo y el servicio
+import { Post } from 'src/app/post.model';
+import { PostService } from 'src/app/post.service';
+
+
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.component.html',
+  styleUrls: ['./show.component.css']
+})
+export class ShowComponent implements OnInit {
+    Posts : Post[]
+    // mientras que el constructor se ejecuta antes.
+  constructor(private postService:PostService) { }
+  /// el onInit ejecuta todo , una vez que la pagina // ya esta renderizada , cuando el componente ha // sido inicializado
+  ngOnInit(): void {
+    //llamamos a nuestro servicio
+    // susbscribe es un metodo que conecta el observer o observador
+    // alert("HOLA SE HA CONECTADO CON FIREBASE!..")
+    // console.log(this.postService.getPosts())
+    this.postService.getPosts().subscribe( (res) =>{
+        this.Posts = res.map ((e)=> {
+            return{
+              id: e.payload.doc.id,
+              // lo que hace es un operador que simplifica la
+           //   recogida de valores en una estructura de datos
+              ...(e.payload.doc.data() as Post)
+            };
+        });
+        console.log(this.Posts)
+    });
+  }
+    deleteRow =(post) => this.postService.deletePost(post);
+
+
+}
